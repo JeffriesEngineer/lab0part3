50f7114eddd9e4522b9a4879bb32a4f2108c810c
natetc95 <natetc95@email.arizona.edu>
2016-02-04 20:41:07 -0700

    nate's version 1
    Major Modifications

--- main.c
+++ main.c
@@ -17,132 +17,93 @@
 
 //TODO: check code and run
 
-typedef enum stateTypeENUM{
-    led1on, led2on, led3on, hold1, hold2, hold3, debounce1, debounce2, debounce3
+typedef enum stateTypeENUM {
+    led1on, led2on, led3on, LT1, GT1, debounce
 } stateType;
 
-volatile stateType state=led1on;
-volatile stateType next;
+volatile stateType state = led1on;
+volatile stateType next; //stores the next possible state based on the current state
+volatile stateType prev; //stores the previous possible state based on the current state
+volatile stateType buffer; //stores the most likely next state based on a button press
 
 //unsigned int timeflag = 0;
 
 int main() {
-    SYSTEMConfigPerformance(10000000);    //Configures low-level system parameters for 10 MHz clock
-    enableInterrupts();                   //This function is necessary to use interrupts.
-
+    SYSTEMConfigPerformance(10000000); //Configures low-level system parameters for 10 MHz clock
+    enableInterrupts(); //This function is necessary to use interrupts.
     //initialization functions
     initLEDs();
     initTimer1();
+    initTimer2();
     initSW1();
-    
-    while(1){
-
-        switch(state)
-        {
-       case led1on:
-            LATDbits.LATD0=1;
-            LATDbits.LATD1=0;
-            LATDbits.LATD2=0;
-            break;
-
-            case debounce1:
-                TMR1=0;
-                IFS0bits.T1IF=0;
-                state=hold1;
+    T1CONbits.ON = 0;
+    while (1) {
+        switch (state) {
+            case led1on:
+                LATDbits.LATD0 = 1;
+                LATDbits.LATD1 = 0;
+                LATDbits.LATD2 = 0;
+                next = led2on;
+                prev = led3on;
                 break;
                 
-            case hold1:
-                delayMs(1);
-                break;
-            
-        case led2on:
-            LATDbits.LATD0=0;
-            LATDbits.LATD1=1;
-            LATDbits.LATD2=0;
-            break;
-             
-            case debounce2:
-                TMR1=0;
-                IFS0bits.T1IF=0;
-                state=hold2;
+            case led2on:
+                LATDbits.LATD0 = 0;
+                LATDbits.LATD1 = 1;
+                LATDbits.LATD2 = 0;
+                next = led3on;
+                prev = led1on;
                 break;
                 
-            case hold2:
-                delayMs(1);
+            case led3on:
+                LATDbits.LATD0 = 0;
+                LATDbits.LATD1 = 0;
+                LATDbits.LATD2 = 1;
+                next = led1on;
+                prev = led2on;
                 break;
                 
-        case led3on:
-            LATDbits.LATD0=0;
-            LATDbits.LATD1=0;
-            LATDbits.LATD2=1;
-            break;
-            
-             case debounce3:
-                TMR1=0;
-                IFS0bits.T1IF=0;
-                state=hold3;
+            case debounce:
+                delayMs(15);
+                state = LT1;
+                buffer = next;
+                TMR1 = 0;
+                T1CONbits.ON = 1;
                 break;
                 
-            case hold3:
-                delayMs(1);
+            case LT1:
+                CNCONDbits.ON = 1;
                 break;
-            
-            defaut:
-            LATDbits.LATD0=0;
-            LATDbits.LATD1=0;
-            LATDbits.LATD2=1;
-            
-            break;
-        
+                
+            case GT1:
+                break;
+                
+            default:
+                LATDbits.LATD0 = 0;
+                LATDbits.LATD1 = 0;
+                LATDbits.LATD2 = 1;
+                next = led1on;
+                prev = led1on;
+                break;
         }
     }
-    
     return 0;
 }
 
+void __ISR(_CHANGE_NOTICE_VECTOR, IPL7SRS) _CNInterrupt(void) {
+    IFS1bits.CNDIF = 0;
+    if(PORTDbits.RD6 == 0) {
+        state = debounce;
+        CNCONDbits.ON = 0;
+    }
+    else if(PORTDbits.RD6 == 1) {
+        state = buffer;
+    }
+}
 
-void __ISR(_CHANGE_NOTICE_VECTOR, IPL7SRS) _CNInterrupt( void ){
-    
-    IFS1bits.CNDIF=0;
-    PORTD;
-    
-    if(state==led1on && PORTDbits.RD6==1)
-        state=debounce1;
-    
-     if(state==led2on && PORTDbits.RD6==1)
-        state=debounce2;
-    
-     if(state==led3on && PORTDbits.RD6==1)
-        state=debounce3;
-    
-    
-    if(state == hold1 && IFS0bits.T1IF==1 && PORTDbits.RD6==0) { // use dummy variable (flag) that is triggered when the timer reaches 1 sec.
-        IFS0bits.T1IF=0;
-        state = led3on;
-    }
-    
-    else if(state == hold1 && PORTDbits.RD6==0)
-        state = led2on;
-    
-    
-    
-       if(state == hold2 && IFS0bits.T1IF==1 && PORTDbits.RD6==0) { // use dummy variable (flag) that is triggered when the timer reaches 1 sec.
-        IFS0bits.T1IF=0;
-        state = led1on;
-    }
-    
-    
-    else if(state == hold2 && PORTDbits.RD6==0)
-        state = led3on;
-      
-       
-    
-        if(state == hold3 && IFS0bits.T1IF==1 && PORTDbits.RD6==0) { // use dummy variable (flag) that is triggered when the timer reaches 1 sec.
-        IFS0bits.T1IF=0;
-        state = led2on;
-    }
-    
-    else if(state == hold3 && PORTDbits.RD6==0)
-        state = led1on;
-    
+void __ISR(_TIMER_1_VECTOR, IPL6SRS) _T1Interrupt() {
+    IFS0bits.T1IF = 0;
+    state = GT1;
+    buffer = prev;
+    T1CONbits.ON = 0;
 }
--- timer.c
+++ timer.c
@@ -9,12 +9,12 @@
 
 void initTimer1(){
     TMR1 = 0;
-    PR1 = 39061;
+    PR1 = 20000;
     T1CONbits.TCKPS = 3;
     T1CONbits.TCS = 0;
-   // IEC0bits.T1IE = 1;
+    IEC0bits.T1IE = 1;
     IFS0bits.T1IF = 0; //timer flag
-   // IPC1bits.T1IP = 7;
+    IPC1bits.T1IP = 6;
     T1CONbits.ON = 1;
 }
 
@@ -23,12 +23,13 @@ initTimer2(){
     T2CONbits.TCKPS = 3;
     T2CONbits.TCS = 0;
     IFS0bits.T2IF = 0;
+    IEC0bits.T2IE = 1;
 }
 
 delayMs(unsigned int delay){
+    IFS0bits.T2IF = 0;
     TMR2 = 0;
     PR2 = delay*624;
-    IFS0bits.T2IF = 0;
     T2CONbits.ON = 1;
     while(IFS0bits.T2IF == 0);
     T2CONbits.ON = 0;
